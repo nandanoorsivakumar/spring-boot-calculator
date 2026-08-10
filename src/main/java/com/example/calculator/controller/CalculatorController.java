59be0b17cdd7f7bf3f48bb2d2ba5b8696deba8b0
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-10 21:54:40 +0530

    Kuber Versioni 19 Added

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 18 Message API Executed");
-        return "Version 18 deployed from Jenkins Docker Pipeline";
+        log.info("Version 19 Message API Executed");
+        return "Version 19 deployed from Jenkins Docker Pipeline";
          }
 
     @GetMapping("/subtract")
