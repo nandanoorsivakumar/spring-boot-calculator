2af120fc8e110c9010c3bca53c0b73bffd5a1051
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-10 18:07:45 +0530

    Added versu 3.9

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 3.8 Message API Executed");
-        return "Version 3.8 deployed from Jenkins Docker Pipeline";
+        log.info("Version 3.9 Message API Executed");
+        return "Version 3.9 deployed from Jenkins Docker Pipeline";
     }
 
     @GetMapping("/subtract")
