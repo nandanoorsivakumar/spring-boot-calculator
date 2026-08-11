ade90026fd8e0d2d5015b89624733e7a3a8c584b
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-11 22:24:44 +0530

    Kubernet version 22

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 20 Message API Executed");
-        return "Version 20 deployed from Jenkins Docker Pipeline";
+        log.info("Version 21 Message API Executed");
+        return "Version 21 deployed from Jenkins Docker Pipeline";
          }
 
     @GetMapping("/subtract")
