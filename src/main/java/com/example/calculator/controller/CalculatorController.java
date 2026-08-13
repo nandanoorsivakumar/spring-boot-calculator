2e0fbbfe4a6596d171a38318b5f1b4d95a589262
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-13 17:37:16 +0530

    Versing 24 added

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 23 Message API Executed");
-        return "Version 23 deployed from Jenkins Docker Pipeline";
+        log.info("Version 24 Message API Executed");
+        return "Version 24 deployed from Jenkins Docker Pipeline";
          }
 
     @GetMapping("/subtract")
