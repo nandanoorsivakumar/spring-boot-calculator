108534ab7ced8d36127b81c963f73583a3fce2ec
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-17 17:11:12 +0530

    Version 34 Added

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 33 Message API Executed");
-        return "Version 33 deployed from Jenkins Docker Pipeline";
+        log.info("Version 34 Message API Executed");
+        return "Version 34 deployed from Jenkins Docker Pipeline";
          }
 
     @GetMapping("/subtract")
