fa210f7958681d678bb09120d68111fa29f4761d
Sivakumar <nandanoorsivakumar@gmail.com>
2026-08-17 15:42:58 +0530

    Version 30 Aded

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -30,8 +30,8 @@ public class CalculatorController {
 
     @GetMapping("/message")
     public String msg() {
-        log.info("Version 29 Message API Executed");
-        return "Version 29 deployed from Jenkins Docker Pipeline";
+        log.info("Version 30 Message API Executed");
+        return "Version 30 deployed from Jenkins Docker Pipeline";
          }
 
     @GetMapping("/subtract")
